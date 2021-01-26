fabcb5bc39fbeffb49e8695e83c6ab1a3e1461ea
dantan123 <dtan9988@gmail.com>
2021-01-26 23:53:59 +0000

    implement counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    // console.log($(this).val());
+    let tweetLength = $(this).val().length;
+    $(".counter").text(140 - tweetLength);
+    if (tweetLength >= 140) {
+      $(".counter").addClass('over-limit');
+    } else {
+      $(".counter").removeClass('over-limit');
+    }
+  })
+})
